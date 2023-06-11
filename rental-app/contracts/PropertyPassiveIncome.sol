ebc94a763290cc5e342fb625b033fb9735cb9a3b
your_username <stztrujillo@hotmail.com>
2023-06-11 10:58:36 -0500

    created property with no implementation

---
+++ rental-app/contracts/PropertyPassiveIncome.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./FactoryProperty.sol";
+import "./Bank.sol";
+
+contract PropertyPassiveIncome is ERC1155 {
+
+ 
+}

f9f55c6fbaa2e52dc39a8cca9b3ec22cdd03a0f8
your_username <stztrujillo@hotmail.com>
2023-06-11 10:57:32 -0500

    created factory of erc20s (properties)

--- rental-app/contracts/FactoryProperty.sol
+++ rental-app/contracts/FactoryProperty.sol
@@ -2,36 +2,65 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "./Property.sol";
+import "./PropertyLoan.sol";
+import "./PropertyPassiveIncome.sol";
 import "./Bank.sol";
 
 
 contract FactoryProperty is AccessControl {
 
-    bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE"); //can mint new properties
+    //~~~~ IMMUTABLE VARIABLES ~~~~
+    Bank immutable private s_bank; //bank will be deployed from this contract to keep it connected as a deployer.
     
+    //~~~~ STATE VARIABLES ~~~~
+    bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE"); //can create new properties
+    mapping(address => bool) private s_collectionsCreated; //collections created by this factory contract
+
     constructor(){
         _grantRole(DEFAULT_ADMIN_ROLE,msg.sender);
         _grantRole(CREATOR_ROLE, msg.sender);
+        s_bank = new Bank();
     }
 
-    /**
-     * 
-     * @param _address address of the user that will be a creator.
-     * 
-     * NOTE: Only creators can create new properties.
-     */
     function addCreator(address  _address) public onlyRole(DEFAULT_ADMIN_ROLE) {
         _grantRole(CREATOR_ROLE, _address);
     }
 
-    function createNewProperty(Property.Option _option, uint256 _totalSupply, uint256 _initialPrice, uint256 _rate, uint256 _valueBackup, address _receiver) public onlyRole(CREATOR_ROLE) {
-        //creation of new erc1155(property)
-        Property _newProperty = new Property(_option, _totalSupply, _initialPrice, _rate, _valueBackup, _receiver);
-
-        //communicate with the bank to register this new property
-
+    //~~~~ EXTERNAL FUNCTIONS(2) ~~~~
+    function createNewPropertyLoan(string memory _name, string memory _symbol, uint256 _totalSupply, uint256 _initialPricePerPiece, uint256 _valueBackup, uint256 _duration, uint256 _totalRate,  address _receiver) external onlyRole(CREATOR_ROLE) {        
+            _registerCollection(address(_createPropertyLoan(_name, _symbol, _totalSupply, _initialPricePerPiece, _valueBackup, _duration, _totalRate, _receiver)));
     }
 
+    function createNewPropertyLoan()  external onlyRole(CREATOR_ROLE) {
+        //TO DO
+        _registerCollection(address(_createPropertyPassiveIncome()));
+    }
 
+    
+    //~~~~ INTERNAL FUNCTIONS(3) ~~~~
+    function _createPropertyLoan(string memory _name, string memory _symbol, uint256 _totalSupply, uint256 _initialPricePerPiece, uint256 _valueBackup, uint256 _duration, uint256 _totalRate, address _receiver) internal returns(address){
+        //creation of new erc1155(property)
+        PropertyLoan _newProperty = new PropertyLoan(_name, _symbol, _totalSupply, _initialPricePerPiece, _valueBackup, _duration, _totalRate, _receiver, bankAddress());
+        return address(_newProperty);
+    }
+
+    function _createPropertyPassiveIncome() internal returns(address){
+        //TO DO
+        PropertyPassiveIncome _newProperty = new PropertyPassiveIncome();
+    }
+
+    function _registerCollection(address _collection) internal{
+        s_collectionsCreated[_collection] = true;
+    }
+
+    //~~~~ VIEW FUNCTIONS(2) ~~~~
+    function bankAddress() public view returns(address){
+        return address(s_bank);
+    }
+
+    function collectionCreated(address _collection) public view returns(bool){
+        return s_collectionsCreated[_collection];
+    }
+
+    
 }

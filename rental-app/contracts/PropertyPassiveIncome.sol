ac4251523b606255b785ab8f80bc8cf3e6c5ff2e
your_username <stztrujillo@hotmail.com>
2023-06-11 19:22:00 -0500

    edited erc1155 to erc20

--- rental-app/contracts/PropertyPassiveIncome.sol
+++ rental-app/contracts/PropertyPassiveIncome.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./FactoryProperty.sol";
 import "./Bank.sol";
 
-contract PropertyPassiveIncome is ERC1155 {
+contract PropertyPassiveIncome is ERC20 {
 
- 
+    constructor() ERC20("",""){
+
+    }
 }

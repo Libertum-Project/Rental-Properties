a2e464d8c4c349b7666439e420baae45e4aeddf8
Christopher Tham <chriswt91@gmail.com>
2023-07-15 20:26:20 -0700

    Add deployment tests

--- test/CapitalRepaymentProperty.t.js
+++ test/CapitalRepaymentProperty.t.js
@@ -6,8 +6,23 @@ describe("CapitalRepaymentProperty", function () {
     const CapitalRepaymentProperty = await ethers.getContractFactory(
       "CapitalRepaymentProperty"
     );
-    const capitalRepaymentProperty = await CapitalRepaymentProperty.deploy();
+    const capitalRepaymentProperty = await CapitalRepaymentProperty.deploy(
+      "Test Property",
+      "TP",
+      1000,
+      100,
+      100000,
+      12,
+      5
+    );
 
     return capitalRepaymentProperty;
   }
+
+  describe("Deployment", function () {
+    it("Should deploy the contract", async function () {
+      const capitalRepaymentProperty = await deployCapitalRepaymentFixture();
+      expect(capitalRepaymentProperty.address).to.not.equal(0);
+    });
+  });
 });

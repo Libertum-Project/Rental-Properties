05e00f16a350c73c61065092a9499c969a9b536c
Christopher Tham <chriswt91@gmail.com>
2023-07-15 23:17:22 -0700

    Edit deployment distribution

--- contracts/MockUSDT.sol
+++ contracts/MockUSDT.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockUSDT is ERC20 {
     constructor() ERC20("USDT", "USDT") {
-        // Mint 1,000 USDT to the deployer of the contract
-        _mint(msg.sender, 1000 * 10 ** 6);
+        // Mint 10k USDT to the deployer of the contract
+        _mint(msg.sender, 10_000 * 10 ** 6);
     }
 
     function faucet(uint256 quantity) external {

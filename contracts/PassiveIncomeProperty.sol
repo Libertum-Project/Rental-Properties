639707c7e9bf63cfc8e18eeed2ba24d26808d82b
Christopher Tham <chriswt91@gmail.com>
2023-07-16 14:25:04 -0700

    Add constructor logic

--- contracts/PassiveIncomeProperty.sol
+++ contracts/PassiveIncomeProperty.sol
@@ -6,5 +6,27 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PassiveIncomeProperty is ERC721, Ownable {
-    constructor() ERC721("PassiveIncomeProperty", "PIP") {}
+    uint256 currentToken;
+    uint256 totalSupply;
+    uint256 pricePerToken;
+    uint256 collateralizedValue;
+    uint256 interestRate;
+
+    IERC20 public paymentToken;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 _totalSupply,
+        uint256 _pricePerToken,
+        uint256 _collateralizedValue,
+        uint256 _interestRate,
+        address paymentTokenAddress
+    ) ERC721(name, symbol) {
+        totalSupply = _totalSupply;
+        pricePerToken = _pricePerToken;
+        collateralizedValue = _collateralizedValue;
+        interestRate = _interestRate;
+        paymentToken = IERC20(paymentTokenAddress);
+    }
 }

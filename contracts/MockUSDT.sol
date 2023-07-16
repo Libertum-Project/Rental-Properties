d005fe73ea5b08caed0dee53123b238366362b8a
Christopher Tham <chriswt91@gmail.com>
2023-07-16 13:47:17 -0700

    Remove auto-faucet on deployment

--- contracts/MockUSDT.sol
+++ contracts/MockUSDT.sol
@@ -4,10 +4,7 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockUSDT is ERC20 {
-    constructor() ERC20("USDT", "USDT") {
-        // Mint 10k USDT to the deployer of the contract
-        _mint(msg.sender, 10_000 * 10 ** 6);
-    }
+    constructor() ERC20("USDT", "USDT") {}
 
     function faucet(uint256 quantity) external {
         _mint(msg.sender, quantity * 10**6);

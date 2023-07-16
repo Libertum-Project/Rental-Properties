f1bcffd46105bee4b232cdedb8f456ec7fecc806
Christopher Tham <chriswt91@gmail.com>
2023-07-16 13:47:31 -0700

    Add test for successful withdrawal from contract

--- test/CapitalRepaymentProperty.t.js
+++ test/CapitalRepaymentProperty.t.js
@@ -50,6 +50,9 @@ describe("CapitalRepaymentProperty", function () {
       const { owner, capitalRepaymentProperty, mockUSDT } =
         await deployCapitalRepaymentFixture();
 
+      // Mint 1k USDT for the owner
+      mockUSDT.connect(owner).faucet(1_000);
+
       // Approve USDT for the contract
       await mockUSDT
         .connect(owner)
@@ -69,6 +72,9 @@ describe("CapitalRepaymentProperty", function () {
       const { owner, capitalRepaymentProperty, mockUSDT } =
         await deployCapitalRepaymentFixture();
 
+      // Mint 5k USDT for the owner
+      mockUSDT.connect(owner).faucet(5_000);
+
       // Approve USDT for the contract
       await mockUSDT
         .connect(owner)
@@ -85,8 +91,11 @@ describe("CapitalRepaymentProperty", function () {
     });
 
     it("Should revert if the user has not approved the contract to spend the required amount", async function () {
-      const { owner, capitalRepaymentProperty } =
+      const { owner, mockUSDT, capitalRepaymentProperty } =
         await deployCapitalRepaymentFixture();
+
+      // Mint 1k USDT for the owner
+      mockUSDT.connect(owner).faucet(1_000);
 
       // Mint 1 NFT for 1000 USDT
       await expect(capitalRepaymentProperty.mint(1)).to.be.revertedWith(
@@ -111,6 +120,34 @@ describe("CapitalRepaymentProperty", function () {
       // Mint 101 NFTs (1 over total supply)
       await expect(capitalRepaymentProperty.mint(101)).to.be.revertedWith(
         "CapitalRepaymentProperty: tokens sold out"
+      );
+    });
+  });
+
+  describe("Withdrawals", function () {
+    it("Should allow the owner to withdraw USDT held on the contract", async function () {
+      const { owner, mockUSDT, capitalRepaymentProperty } =
+        await deployCapitalRepaymentFixture();
+
+      // Mint 1k USDT for the owner
+      mockUSDT.connect(owner).faucet(1_000);
+
+      // Mint 1 NFT for 1000 USDT
+      await mockUSDT
+        .connect(owner)
+        .approve(
+          capitalRepaymentProperty.address,
+          ethers.utils.parseUnits("1000", 6)
+        );
+      await capitalRepaymentProperty.mint(1);
+
+      // Verify that owner's USDT balance is zero
+      expect(await mockUSDT.balanceOf(owner.address)).to.equal(0);
+
+      // Withdraw USDT and verify owner's new USDT balance
+      await capitalRepaymentProperty.withdraw();
+      expect(await mockUSDT.balanceOf(owner.address)).to.equal(
+        ethers.utils.parseUnits("1000", 6)
       );
     });
   });

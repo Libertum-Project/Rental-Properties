039b743202bfc624b52108db7b796bdd7bd5441f
Christopher Tham <chriswt91@gmail.com>
2023-07-15 22:57:45 -0700

    Add name and symbol deployment tests

--- test/CapitalRepaymentProperty.t.js
+++ test/CapitalRepaymentProperty.t.js
@@ -3,6 +3,11 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("CapitalRepaymentProperty", function () {
   async function deployCapitalRepaymentFixture() {
+    const [owner] = await ethers.getSigners();
+
+    const MockUSDT = await ethers.getContractFactory("MockUSDT");
+    const mockUSDT = await MockUSDT.deploy();
+
     const CapitalRepaymentProperty = await ethers.getContractFactory(
       "CapitalRepaymentProperty"
     );
@@ -13,16 +18,51 @@ describe("CapitalRepaymentProperty", function () {
       100,
       100000,
       12,
-      5
+      500,
+      mockUSDT.address
     );
 
-    return capitalRepaymentProperty;
+    return { owner, capitalRepaymentProperty, mockUSDT };
   }
 
   describe("Deployment", function () {
     it("Should deploy the contract", async function () {
-      const capitalRepaymentProperty = await deployCapitalRepaymentFixture();
+      const { capitalRepaymentProperty } =
+        await deployCapitalRepaymentFixture();
       expect(capitalRepaymentProperty.address).to.not.equal(0);
+    });
+
+    it("Should set the correct name", async function () {
+      const { capitalRepaymentProperty } =
+        await deployCapitalRepaymentFixture();
+      expect(await capitalRepaymentProperty.name()).to.equal("Test Property");
+    });
+
+    it("Should set the correct symbol", async function () {
+      const { capitalRepaymentProperty } =
+        await deployCapitalRepaymentFixture();
+      expect(await capitalRepaymentProperty.symbol()).to.equal("TP");
+    });
+  });
+
+  describe("Minting", function () {
+    it("Should allow users to mint tokens for 100 USDT", async function () {
+      const { owner, capitalRepaymentProperty, mockUSDT } =
+        await deployCapitalRepaymentFixture();
+
+      // Approve USDT for the contract
+      await mockUSDT
+        .connect(owner)
+        .approve(
+          capitalRepaymentProperty.address,
+          ethers.utils.parseUnits("100", 6)
+        );
+
+      // Mint 1 NFT for 100 USDT
+      await capitalRepaymentProperty.mint(1);
+      expect(await capitalRepaymentProperty.balanceOf(owner.address)).to.equal(
+        1
+      );
     });
   });
 });

1a93ff5d0510703eb8930ec2a6f034ed8c6f871b
Christopher Tham <chriswt91@gmail.com>
2023-07-16 14:27:36 -0700

    Add mint function

--- contracts/PassiveIncomeProperty.sol
+++ contracts/PassiveIncomeProperty.sol
@@ -29,4 +29,33 @@ contract PassiveIncomeProperty is ERC721, Ownable {
         interestRate = _interestRate;
         paymentToken = IERC20(paymentTokenAddress);
     }
+
+    function mint(uint256 quantity) external {
+        // Check that current token is less than total supply and the correct USD amount
+        // has been transferred by the user, we are checking outside of the minting loop
+        // as a fail-fast mechanism.
+        require(
+            currentToken < totalSupply,
+            "PassiveIncomeProperty: tokens sold out"
+        );
+        require(
+            // NOTE: 10**6 is the number of decimals for USDT (and most other stablecoins)
+            paymentToken.transferFrom(
+                msg.sender,
+                address(this),
+                pricePerToken * quantity * 10 ** 6
+            ),
+            "PassiveIncomeProperty: purchase failed"
+        );
+
+        // Mint the correct quantity of tokens to the user and increment token counter
+        for (uint256 i = 0; i < quantity; i++) {
+            require(
+                currentToken < totalSupply,
+                "PassiveIncomeProperty: tokens sold out"
+            );
+            _safeMint(msg.sender, currentToken);
+            currentToken++;
+        }
+    }
 }

db1b60455cc4331360af8337e2bf43009bf1cce2
Christopher Tham <chriswt91@gmail.com>
2023-07-15 19:49:08 -0700

    Add property variables

--- contracts/CapitalRepaymentProperty.sol
+++ contracts/CapitalRepaymentProperty.sol
@@ -4,8 +4,25 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract CapitalRepaymentProperty is ERC721 {
+    uint256 totalSupply;
+    uint256 initialPrice;
+    uint256 collateralizedValue;
+    uint256 durationInMonths;
+    uint256 interestRate;
+
     constructor(
         string memory name,
-        string memory symbol
-    ) ERC721(name, symbol) {}
+        string memory symbol,
+        uint256 _totalSupply,
+        uint256 _initialPrice,
+        uint256 _collateralizedValue,
+        uint256 _durationInMonths,
+        uint256 _interestRate
+    ) ERC721(name, symbol) {
+        totalSupply = _totalSupply;
+        initialPrice = _initialPrice;
+        collateralizedValue = _collateralizedValue;
+        durationInMonths = _durationInMonths;
+        interestRate = _interestRate;
+    }
 }

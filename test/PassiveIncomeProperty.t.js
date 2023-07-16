6662752e0d955861162596b69f55bb55b8fd1f00
Christopher Tham <chriswt91@gmail.com>
2023-07-16 14:31:09 -0700

    Add deployment unit tests

--- test/PassiveIncomeProperty.t.js
+++ test/PassiveIncomeProperty.t.js
@@ -23,4 +23,24 @@ describe("PassiveIncomeProperty", function () {
 
     return { owner, user, passiveIncomeProperty, mockUSDT };
   }
+
+  describe("Deployment", function () {
+    it("Should deploy the contract", async function () {
+      const { passiveIncomeProperty } =
+        await deployPassiveIncomeFixture();
+      expect(passiveIncomeProperty.address).to.not.equal(0);
+    });
+
+    it("Should set the correct name", async function () {
+      const { passiveIncomeProperty } =
+        await deployPassiveIncomeFixture();
+      expect(await passiveIncomeProperty.name()).to.equal("Test Property");
+    });
+
+    it("Should set the correct symbol", async function () {
+      const { passiveIncomeProperty } =
+        await deployPassiveIncomeFixture();
+      expect(await passiveIncomeProperty.symbol()).to.equal("TP");
+    });
+  });
 });

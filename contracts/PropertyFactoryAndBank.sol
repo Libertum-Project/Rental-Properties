d33f8c00e906e83a0201594a9089375004f85667
Christopher Tham <chriswt91@gmail.com>
2023-07-16 17:35:17 -0700

    Add base PropertyFactoryAndBank contract

---
+++ contracts/PropertyFactoryAndBank.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "./CapitalRepaymentProperty.sol";
+import "./PassiveIncomeProperty.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PropertyFactoryAndBank is Ownable {
+    constructor() {}
+}

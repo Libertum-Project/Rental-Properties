3c7e5c3b121d39c347cd8ed79450747b84dea069
Christopher Tham <chriswt91@gmail.com>
2023-07-16 17:48:48 -0700

    Add newCapitalRepaymentProperty function

--- contracts/PropertyFactoryAndBank.sol
+++ contracts/PropertyFactoryAndBank.sol
@@ -6,5 +6,33 @@ import "./PassiveIncomeProperty.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PropertyFactoryAndBank is Ownable {
+    address[] public capitalRepaymentProperties;
+
     constructor() {}
+
+    function newCapitalRepaymentProperty(
+        string memory name,
+        string memory symbol,
+        uint256 totalSupply,
+        uint256 pricePerToken,
+        uint256 collateralizedValue,
+        uint256 durationInMonths,
+        uint256 interestRate,
+        address paymentTokenAddress
+    ) external onlyOwner returns (address) {
+        CapitalRepaymentProperty property = new CapitalRepaymentProperty(
+            name,
+            symbol,
+            totalSupply,
+            pricePerToken,
+            collateralizedValue,
+            durationInMonths,
+            interestRate,
+            paymentTokenAddress
+        );
+
+        capitalRepaymentProperties.push(address(property));
+
+        return address(property);
+    }
 }

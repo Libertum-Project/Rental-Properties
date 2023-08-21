b500b8e3910602439c2201fecc1fbefb191d0e7a
Christopher Tham <chriswt91@gmail.com>
2023-08-21 15:41:24 -0700

    Add mumbai network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,15 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 10,
       },
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  networks: {
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY!],
     },
   },
 };

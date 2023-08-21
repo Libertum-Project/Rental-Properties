48bcb6ba3bb2537fd5bcb453d5f196050d180f10
Christopher Tham <chriswt91@gmail.com>
2023-08-21 15:57:05 -0700

    Match variable name to network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
     }
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: process.env.POLYGONSCAN_API_KEY,
   },
   networks: {
     mumbai: {
